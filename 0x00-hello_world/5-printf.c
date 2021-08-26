5ecfc113602d9f13b49a077bac6535dbb9639bc4
nandweza <allannandweza@gmail.com>
2021-08-26 09:15:32 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sentence with proper grammar,
+ * but the outcome is a piece of art, 
+ * return: 0 if exited properlyly, non-zero otherwise
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
