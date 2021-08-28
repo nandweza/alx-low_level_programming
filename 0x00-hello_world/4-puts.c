0fe0f689a9d5436f1858144dfe5c780294eadf9d
nandweza <allannandweza@gmail.com>
2021-08-28 06:47:56 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
+ * main - prints the phrase "programming is like building a multilingual puzzle
  * Return: 0 if exited properly
  */
 int main(void)
