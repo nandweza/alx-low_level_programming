153a8a4e07dd65b8df74c63fb1a1033701757aad
nandweza <allannandweza@gmail.com>
2021-08-31 08:45:02 -0700

    task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints Hlberton, followed by a new line.
+ * main - prints Holberton, followed by a new line.
  * Return: Always 0 (success)
  */
 
@@ -10,8 +10,8 @@ int main(void)
 	char c[10] = "Holberton";
 	int a;
 
-	for (a = 0; a < 10; a++;){
-		_putchar('a');
+	for(a = 0; a < 10; a++){
+		_putchar(c[a]);
 	}
 	_putchar('\n');
 	return (0);
