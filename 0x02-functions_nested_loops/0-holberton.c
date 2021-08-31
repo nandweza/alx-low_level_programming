3765093b204205964fbea8192c3d82529be20504
nandweza <allannandweza@gmail.com>
2021-08-31 08:29:45 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints Hlberton, followed by a new line.
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c[10] = "Holberton";
+	int a;
+
+	for (a = 0; a < 10; a++;){
+		_putchar('a');
+	}
+	_putchar('\n');
+	return (0);
+}
