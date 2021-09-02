73a97f61797922bb7245f070b02ff2fe49c25695
nandweza <allannandweza@gmail.com>
2021-09-02 06:10:52 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -34,17 +34,5 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-		if (year % 400 == 0)
-		{
-			printf("%d is a leap year.", year);
-		}
-		else if (year % 100 == 0)
-		{
-			printf("%d is a leap year.", year);
-		}
-		else
-		{
-			printf("%d is not a leap year.", year);
-		}
 	}
 }
