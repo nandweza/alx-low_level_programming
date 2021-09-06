354da063415b4212417b5944d61407ee0b9bb1d9
nandweza <allanndweza@gmail.com>
2021-09-06 07:47:29 -0700

    added task 6 and header

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints one character out of 2 of a string,
+ * followed by a new line.
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int len a;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (a = 0; a < len; a += 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
