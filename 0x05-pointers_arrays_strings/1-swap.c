ef17761a96265fef292240cd655053735d319f26
nandweza <allanndweza@gmail.com>
2021-09-06 02:10:33 -0700

    added header and task 1 to 0x05

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - prints a function that swaps the values of two integers
+ *@a: variable
+ *@b: variable
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
