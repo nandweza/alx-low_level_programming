e28ef633378f2a01b6eca6118e11cc5b10635f77
nandweza <allanndweza@gmail.com>
2021-09-06 03:46:36 -0700

    changes to task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,8 +3,15 @@
 /**
  * _puts - prints a string, followed by a new line,
  * to stdout.
- */
+ * @str: pointer to the string to print
+*/
 void _puts(char *str)
 {
-	_puts(str);
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
 }
