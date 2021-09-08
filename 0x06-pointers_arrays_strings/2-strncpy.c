a4b64be81a867ccf12844bbc0aa98eef14f0856f
nandweza <allanndweza@gmail.com>
2021-09-08 07:35:44 -0700

    added header and task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy a string.
+ * @n: number of bytes to copy
+ * @dest: destination string
+ * @src: source string
+ * Return: a pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
