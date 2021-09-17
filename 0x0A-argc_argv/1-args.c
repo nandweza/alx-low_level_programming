70671fad2d8170671b6a905f673d41b3ea9844af
nandweza <allanndweza@gmail.com>
2021-09-17 08:38:31 -0700

    correction to task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,8 @@
  *
  * Return: Always 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
