ac7573c4a5da5f0deca33deb2ed79fbc4be73d4f
nandweza <allannandweza@gmail.com>
2021-11-26 03:51:33 -0800

    modifications

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,16 +20,20 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
+		new->prev = NULL;
 		*head = new;
 		return (new);
 	}
 
 	while (tmp->next)
 		tmp = tmp->next;
+
 	tmp->next = new;
+	new->prev = tmp;
 
 	return (new);
 }
